a90aa8b96d58a7da1c7e311610a9b3e8c5d7cccb
wahyubudii <wahyubudies@student.ub.ac.id>
2021-07-01 10:11:04 +0700

    initial init

---
+++ database/factories/VaksinFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Vaksin;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VaksinFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Vaksin::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
